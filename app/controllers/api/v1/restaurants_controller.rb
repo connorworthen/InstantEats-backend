434692467740ccc70fc394f6173cd5952ddf677d
connorworthen <connorw220@protonmail.com>
2020-12-31 15:38:45 -0800

    restaurant strong params

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::RestaurantsController < ApplicationController
+
+  def index
+    
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :category, :price, :hours, :distance)
+  end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+restaurant = Restaurant.create(name: 'In N Out', category: 'Fast Food', price: '$', hours: '10:30 AM - 1:00 AM', distance: 2.3)
+
+user = User.create(first_name: 'Connor', last_name: 'Worthen', email: 'connorworthen@icloud.com', password: 'Test1', address: '601 S Twin Oaks Valley Road', phone_number: '760-468-7537')
