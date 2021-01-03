83951d3a45d3f1023ee48b4fb5e0b1f2d03f5fae
connorworthen <connorw220@protonmail.com>
2021-01-02 23:09:10 -0800

    commented out validations error validations on frontend

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1:UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
 
   def index
     @users = User.all
@@ -31,7 +31,7 @@ class Api::V1:UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      login!
+      # login!
       render json: {
         status: :created,
         user: @user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :first_name, presence: true
-  validates :first_name, uniqueness: true
-  validates :last_name, presence: true
-  validates :last_name, uniqueness: true
-  validates :email, presence: true
-  validates :email, uniqueness: true
-  validates :address, presence: true
-  validates :address, uniqueness: true
-  validates :phone_number, presence: true
-  validates :phone_number, uniqueness: true 
+  # validates :first_name, presence: true
+  # validates :first_name, uniqueness: true
+  # validates :last_name, presence: true
+  # validates :last_name, uniqueness: true
+  # validates :email, presence: true
+  # validates :email, uniqueness: true
+  # validates :address, presence: true
+  # validates :address, uniqueness: true
+  # validates :phone_number, presence: true
+  # validates :phone_number, uniqueness: true 
 
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  # validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
