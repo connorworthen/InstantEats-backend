1c9ca79ac061dd9ecc48c2f5864c0b519ad556f3
connorworthen <connorw220@protonmail.com>
2021-01-03 13:19:06 -0800

    logged in now custom route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  get '/logged_in', to: 'sessions#is_logged_in?'
-
   namespace :api do
     namespace :v1 do
+      resources :sessions
+      get :logged_in, to: 'sessions#is_logged_in?'
       resources :users, only: [:create, :show, :index]
       resources :restaurants
     end
