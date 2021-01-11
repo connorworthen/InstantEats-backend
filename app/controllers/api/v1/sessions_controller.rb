5b226df767aaf46386eff2c6f8555eb24ea37cea
connorworthen <connorw220@protonmail.com>
2021-01-10 17:07:35 -0800

    fixed sessions(login) method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,16 +2,9 @@ class Api::V1::SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: session_params[:email])
-  
     if @user && @user.authenticate(session_params[:password])
-      # login!
-      payload = {user_id: user.id}
-      token = encode_token(payload)
-      render json: {
-        logged_in: true,
-        user: @user,
-        jwt: token
-      }
+      render json: @user
+      # binding.pry
     else
       render json: { 
         status: 401,
@@ -20,21 +13,21 @@ class Api::V1::SessionsController < ApplicationController
     end
   end
 
-  def is_logged_in?
-    # if logged_in? && current_user
-    if session_user
-      render json: {
-        session_user
-        # logged_in: true,
-        # user: current_user
-      }
-    else
-      render json: {
-        logged_in: false,
-        message: 'no such user'
-      }
-    end
-  end
+  # def is_logged_in?
+  #   # if logged_in? && current_user
+  #   if session_user
+  #     render json: {
+  #       session_user
+  #       # logged_in: true,
+  #       # user: current_user
+  #     }
+  #   else
+  #     render json: {
+  #       logged_in: false,
+  #       message: 'no such user'
+  #     }
+  #   end
+  # end
 
   def destroy
     logout!
@@ -47,7 +40,7 @@ class Api::V1::SessionsController < ApplicationController
   private
 
   def session_params
-    params.require(:user).permit(:email, :password)
+    params.permit(:email, :password)
   end
 
   def logged_in?
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      resources :sessions
+      resources :sessions, only: [:create, :show, :index]
       get :logged_in, to: 'sessions#is_logged_in?'
       resources :users, only: [:create, :show, :index]
       resources :restaurants
