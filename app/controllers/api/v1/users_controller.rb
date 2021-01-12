3ce91961dae6e7f4455935fa5b3333c9d62a3a41
connorworthen <connorw220@protonmail.com>
2021-01-11 19:36:57 -0800

    jwt setup + login moved to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
-  
+  before_action :authorized, only: [:auto_login]
+
   def index
     render json: User.all
   end
@@ -10,24 +11,29 @@ class Api::V1::UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: @user
-    else 
-      render json: {error: 'Error creating account. Please try again.'}
+    @user = User.create(user_params)
+    if @user.valid?
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+    else
+      render json: {error: "Invalid username or password"}
     end
   end
 
   def login
-    @user = User.find_by(email: session_params[:email])
-    if @user && @user.authenticate(session_params[:password])
-      render json: @user
+    @user = User.find_by(email: params[:email])
+
+    if @user && @user.authenticate(params[:password])
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
     else
-      render json: { 
-        status: 401,
-        errors: ['no such user', 'verify credentials and try again or signup']
-      }
+      render json: {error: "Invalid username or password"}
     end
+  end
+
+
+  def auto_login
+    render json: @user
   end
 
   private
