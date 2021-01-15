94291566974adcbd5bc9cbfee6c2c23b9023833b
connorworthen <connorw220@protonmail.com>
2021-01-15 14:18:05 -0800

    validations for models set

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,4 @@
 class Restaurant < ApplicationRecord
+  belongs_to :user
+  has_many :menus
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :restaurants
+  has_many :menus, through: :restaurants
+  has_many :items, through: :menus
+
+  validates :first, presence: true
+  validates :last, presence: true
+  validates :email, presence: true, uniqueness: true
 end
